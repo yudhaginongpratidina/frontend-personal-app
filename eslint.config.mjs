3d83b12cc0abe16693fff331dca30f7a2c1fcc86
yudhaginongpratidina <dev.yudhaginongpratidina.com>
2025-05-21 10:38:16 +0700

    21-05-003: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",        // Disable TypeScript 'any' type errors
+            "react-hooks/exhaustive-deps": "off",               // Disable React Hooks dependency warnings
+            "@typescript-eslint/no-unused-vars": "off",         // Disable unused variables errors
+            "@next/next/no-img-element": "off",                 // Disable Next.js img element warnings
+            "react/no-unescaped-entities": "off",               // Disable unescaped entities warnings
+            "prefer-const": "off",                              // Disable prefer const warnings
+            "@typescript-eslint/no-unused-expressions": "off"   // Disable unused expressions warnings
+        }
+    }
 ];
 
 export default eslintConfig;
